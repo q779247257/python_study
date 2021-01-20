35c4c9e2481416e4267d0f9ae0987fd902724871
xuanxuan <xuanxuan@xxx.com>
2021-01-20 16:05:49 +0800

    1

---
+++ Python基础/demo.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+a = 123 # a是一个int类型的数字
+print(a)
+a = 'ANC' # a变为一个字符串
+print(a)
+
+# 这种变量本身类型不固定的语言称为动态语言，与之对应的是静态语言。静态语言在定义变量时必须指定变量类型，如果复制的时候类型不匹配就会报错，
+# 例如java和C##
+
+
+# -*- coding: utf-8 -*-
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 print("1024 * 768 =",1024 * 768)
 print("I\'m \"OK\"!")
+
+
+# 不会进行转移 r''
+print(r'\\\t\\')
+
+
+# 多行内容 不用\t换行
+print('''听说这个玩意不用换行
+我试试是不是真的''')
+
+age = 18
+
+if age == 17 and age == 18:
+    print('true')
+else:
+    print("false")
+    print(None)
+
+
+
